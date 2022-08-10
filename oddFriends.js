ecf63040a7052767c8f2de481829aadf55ca340d
github_username <you@domain.com>
2022-08-10 05:42:17 -0700

    oddfriends

---
+++ oddFriends.js
@@ -0,0 +1,12 @@
+function oddFriends(arr){
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i]%2 === 0){
+            console.log(arr[i]);
+        }  
+        const arr =['rohima', 'situ', 'rohim', 'firoja','alhaj'];
+ 
+    }
+}
+
+
+console.log(oddFriends(arr));
