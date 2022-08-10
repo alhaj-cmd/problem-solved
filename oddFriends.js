0aa53267ceb1e38f60a4a3047b1b9598a2d136fe
github_username <you@domain.com>
2022-08-10 08:59:37 -0700

    complete the odd Friends

--- oddFriends.js
+++ oddFriends.js
@@ -1,12 +1,13 @@
-function oddFriends(arr){
-    for(let i = 0; i < arr.length; i++){
-        if (arr[i]%2 === 0){
-            console.log(arr[i]);
-        }  
-        const arr =['rohima', 'situ', 'rohim', 'firoja','alhaj'];
- 
+function oddFriends(FriendArray){
+   // let oddFriend = 1;
+    for(let i = 0; i < FriendArray.length; i++){
+       
+      if (FriendArray[i].length % 2 != 0 ){
+         return FriendArray[i];
+           
+        }
     }
 }
 
-
-console.log(oddFriends(arr));
+const FriendArray =['rohima', 'situ', 'rohima', 'firoja','alhaj','priya','ali'];
+console.log(oddFriends(FriendArray));
